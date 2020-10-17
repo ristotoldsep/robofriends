2d4fd1d4c5de84969a482ec7ea5e7c615e24b19b
Risto Tõldsep <r.toldsep@gmail.com>
2020-10-18 01:28:23 +0300

    Added jsonplaceholder API for data

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import './App.css'
-import { robots } from './robots'; //Because robots.js doesn't return only 1 object, we have to use brackets!
+// import { robots } from './robots'; //Because robots.js doesn't return only 1 object, we have to use brackets!
 
 // const App = () => {
 class App extends Component {
     constructor() {
         super(); //Just need to add 'this' which calls the constructor of 'Component'
         this.state = { //App.js now owns state and allows to access it from elsewhere
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users/') //Fetching users from API
+            .then(response => response.json()) ////Getting a response and turning data into JSON
+            .then(users => this.setState({ robots: users })); //Then getting users and updating them with setState!
     }
 
     onSearchChange = (event) => {
@@ -27,14 +33,18 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        // console.log(filteredRobots);
-        return (
-            <div className='tc'>
-                <h1 className="f2">RoboFriends</h1>
-                < SearchBox searchChange = {this.onSearchChange}/>        
-                < CardList robots={filteredRobots} />
-            </div>
-        );
+        if(this.state.robots.length === 0) {
+            return <h1 className="tc">Loading</h1>
+        } else {
+            // console.log(filteredRobots);
+            return (
+                <div className='tc'>
+                    <h1 className="f2">RoboFriends</h1>
+                    < SearchBox searchChange = {this.onSearchChange}/>        
+                    < CardList robots={filteredRobots} />
+                </div>
+            );
+        }
     }
 }
 
